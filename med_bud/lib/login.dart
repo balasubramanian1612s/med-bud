c6f5fc50d10efdd3ddefcda87567d0248c42b9fc
Vikneshwar Vr <vikneshwaranandan@gmail.com>
2021-02-28 04:09:03 +0530

    auth form

--- med_bud/lib/login.dart
+++ med_bud/lib/login.dart
@@ -6,6 +6,11 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  String userId, pwd;
+
+  final formKey = GlobalKey<FormState>();
+  bool validated = false;
+  bool autoValidate = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,70 +20,93 @@ class _LoginState extends State<Login> {
         backgroundColor: Colors.pink,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 60.0),
-              child: Center(
-                child: Container(
-                  width: 200,
-                  height: 150,
-                  color: Colors.pink[50],
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 60.0),
+                child: Center(
+                  child: Container(
+                    width: 200,
+                    height: 150,
+                    color: Colors.pink[50],
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              //padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 0, bottom: 0),
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                    hintText: 'Email'),
+              Padding(
+                //padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 0, bottom: 0),
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please fill out this field';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    userId = value;
+                  },
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                      hintText: 'Email'),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 15.0, right: 15.0, top: 15, bottom: 0),
-              //padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter secure password'),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 15.0, right: 15.0, top: 15, bottom: 0),
+                //padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please fill out this field';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    pwd = value;
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      hintText: 'Enter secure password'),
+                ),
               ),
-            ),
-            // ignore: deprecated_member_use
-            FlatButton(
-              onPressed: () {},
-              child: Text(
-                'Forgot Password',
-                style: TextStyle(color: Colors.pink[500], fontSize: 15),
-              ),
-            ),
-            Container(
-              height: 40,
-              width: 150,
-              decoration: BoxDecoration(
-                  color: Colors.pink[300],
-                  borderRadius: BorderRadius.circular(20)),
-              child: FlatButton(
-                onPressed: () {
-                  // Navigator.push(
-                  //     context, MaterialPageRoute(builder: (_) => HomePage()));
-                },
+              // ignore: deprecated_member_use
+              FlatButton(
+                onPressed: () {},
                 child: Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white, fontSize: 20),
+                  'Forgot Password',
+                  style: TextStyle(color: Colors.pink[500], fontSize: 15),
                 ),
               ),
-            ),
-            // SizedBox(
-            //   height: 130,
-            // ),
-            // Text('New User? Create Account')
-          ],
+              Container(
+                height: 40,
+                width: 150,
+                decoration: BoxDecoration(
+                    color: Colors.pink[300],
+                    borderRadius: BorderRadius.circular(20)),
+                child: FlatButton(
+                  onPressed: () {
+                    formKey.currentState.validate();
+                    if (formKey.currentState.validate()) {
+                      formKey.currentState.save();
+                    }
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+              // SizedBox(
+              //   height: 130,
+              // ),
+              // Text('New User? Create Account')
+            ],
+          ),
         ),
       ),
     );
