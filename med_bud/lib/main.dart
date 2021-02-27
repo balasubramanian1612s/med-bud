8741945dbad04c93488267ee90c3efa6a46d105f
Vikneshwar Vr <vikneshwaranandan@gmail.com>
2021-02-28 03:30:54 +0530

    login,signup

--- med_bud/lib/main.dart
+++ med_bud/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:med_bud/pages/remainder_home.dart';
 import 'package:med_bud/pages/schedule_tablets_listing.dart';
 import 'package:med_bud/pages/scheduler_medicine_list.dart';
 import 'package:med_bud/test/notificationPage.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MultiProvider(
         providers: [],
         child: MaterialApp(
@@ -23,7 +23,6 @@ class MyApp extends StatelessWidget {
           ),
           home: MedicineShopping(),
         ));
-
   }
 }
 
